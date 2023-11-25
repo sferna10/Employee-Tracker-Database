32cbf5f9aee95fd86e4a81e307e179e9e1430fcb
sferna10 <sferna10@email.cpcc.edu>
2023-11-25 00:04:52 -0500

    Installing asciiart package

--- db/schema.sql
+++ db/schema.sql
@@ -1,36 +1,41 @@
-CREATE DATABASE employees_db;
-
 DROP DATABASE IF EXISTS employees_db;
+
 CREATE DATABASE employees_db;
 
 USE employees_db;
 
 --See database in use--
+
 SELECT DATABASE();
 
 --Create department--
 
-CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NULL,
-  PRIMARY KEY (id)
-);
+CREATE TABLE
+    department (
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(30) NULL,
+        PRIMARY KEY (id)
+    );
 
 --Create role--
-CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30) NULL,
-  salary DECIMAL(10.3) NULL,
-  department_id INT NULL,
-  PRIMARY KEY (id)
-);
+
+CREATE TABLE
+    role (
+        id INT NOT NULL AUTO_INCREMENT,
+        title VARCHAR(30) NULL,
+        salary DECIMAL(10.3) NULL,
+        department_id INT NULL,
+        PRIMARY KEY (id)
+    );
 
 --Create employee
-CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NULL,
-  last_name VARCHAR(30) NULL,
-  role_id INT NULL,
-  manager_id INT NULL,
-  PRIMARY KEY (id)
-);
+
+CREATE TABLE
+    employee (
+        id INT NOT NULL AUTO_INCREMENT,
+        first_name VARCHAR(30) NULL,
+        last_name VARCHAR(30) NULL,
+        role_id INT NULL,
+        manager_id INT NULL,
+        PRIMARY KEY (id)
+    );
--- starter.js
+++
@@ -1,3 +0,0 @@
-const inquirer = require("inquirer");
-const mysql = require("mysql2")
-const db = require("./db/connections")
