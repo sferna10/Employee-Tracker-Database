e45bdd63a393b13bd98073aec3505532138bc6ae
Silvia A. Fernandez <sferna10@email.cpcc.edu>
2023-12-21 23:05:43 -0500

    Adding code to seed.sql

--- Main/db/seed.sql
+++ Main/db/seed.sql
@@ -3,9 +3,18 @@ USE employees;
 /* === || DEPARTMENT ARRAY || === */
 INSERT INTO department (name)
 VALUES ("Sales");
-
+INSERT INTO department (name)
+VALUES ("Engineering");
+INSERT INTO department (name)
+VALUES ("Finance");
 INSERT INTO department (name)
 VALUES ("Legal");
+
+NSERT INTO department (name)
+VALUES ("HUMAN RESOURCES");
+
+
+
 
 /* === || ROLE ARRAY || === */
 INSERT INTO role (title, salary, department_id)
@@ -13,6 +22,10 @@ VALUES ("Sales Lead", 95000, 1);
 /* Sales Department */
 INSERT INTO role (title, salary, department_id)
 VALUES ("Salesperson", 80000, 1);
+
+INSERT INTO role (title,salary, department_id)
+VALUES ("Technitian, 56000, 1)
+
 
 /
 /* === || EMPLOYEE ARRAY || === */
