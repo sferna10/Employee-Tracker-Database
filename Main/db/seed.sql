d6c6394914fa85943c35c475e3c3965903b789da
Silvia A. Fernandez <sferna10@email.cpcc.edu>
2023-12-22 11:36:06 -0500

    Adding data to tables

--- Main/db/seed.sql
+++ Main/db/seed.sql
@@ -9,9 +9,8 @@ INSERT INTO department (name)
 VALUES ("Finance");
 INSERT INTO department (name)
 VALUES ("Legal");
-
 INSERT INTO department (name)
-VALUES ("HUMAN RESOURCES");
+VALUES ("Human Resources");
 
 
 
@@ -22,10 +21,14 @@ VALUES ("Sales Lead", 95000, 1);
 /* Sales Department */
 INSERT INTO role (title, salary, department_id)
 VALUES ("Salesperson", 80000, 1);
-
 INSERT INTO role (title,salary, department_id)
 VALUES ("Technitian", 56000, 1)
-
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sotware_Engineer", 120000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 125000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant_Manager", 140000, 3);
 
 
 /* === || EMPLOYEE ARRAY || === */
@@ -33,5 +36,14 @@ INSERT INTO employee (first_name, last_name, role_id, manager_id)
 /* John Doe - Sales Lead - Sales Department */
 VALUES ("John", "Doe", 1, null); 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
+/* Silvia Fernandez - Manager - Engineer Department */
+VALUES ("Ale", "Seas", 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+/* Denise Watson - Account Manager- Accounting Department */
+VALUES ("Denise", "Watson", 5, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 /* Silvia Fernandez - Manager - Sales Department */
-VALUES ("Silvia", "Fernandez", 1, 1);
+VALUES ("Nicholas", "Hover", 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+/* Denise Watson - Professional 1- Legal Department */
+VALUES ("Silvia", "Fernandez", 5, null);
